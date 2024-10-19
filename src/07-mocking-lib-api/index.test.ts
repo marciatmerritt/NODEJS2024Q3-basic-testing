c487500eefa076cf80529207c0c56a87c0df327f
Marcia Merritt <marciatmerritt@gmail.com>
2024-10-18 22:16:57 -0400

    implement 07-mocking-lib-api and 08-snapshot-testing testing, update readme

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,51 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('lodash', () => ({
+  throttle: jest.fn((fn) => fn),
+}));
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+    mockedAxios.get.mockResolvedValue({ data: {} });
+    jest.spyOn(axios, 'create').mockReturnValue(mockedAxios);
+
+    await throttledGetDataFromApi('/posts');
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: baseUrl,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    mockedAxios.get.mockResolvedValue({ data: {} });
+    jest.spyOn(axios, 'create').mockReturnValue(mockedAxios);
+
+    const relativePath = '/posts';
+
+    await throttledGetDataFromApi(relativePath);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockData = { id: 1, title: 'Test Post' };
+
+    mockedAxios.get.mockResolvedValue({ data: mockData });
+    jest.spyOn(axios, 'create').mockReturnValue(mockedAxios);
+
+    const relativePath = '/posts/1';
+
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(mockData);
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,22 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const result = generateLinkedList([1]);
+    const expected = {
+      value: 1,
+      next: {
+        value: null,
+        next: null,
+      },
+    };
+    expect(result).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const result = generateLinkedList([1, 2, 3]);
+    expect(result).toMatchSnapshot();
   });
 });
